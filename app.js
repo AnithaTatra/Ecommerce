c09ec0611ea811cd3672abebb284e8b256200125
AnithaTatra <102857170+AnithaTatra@users.noreply.github.com>
2022-05-13 01:02:25 +0530

    commited

--- app.js
+++ app.js
@@ -10,7 +10,23 @@ const cart_route = require('./route/cart.route')
 //server starting code
 const app = express()
 const PORT = process.env.PORT || 3000
- 
+
+
+app.get('/check', function (req, res) {
+    console.log(req.path);
+    console.log("route..",req.route);
+    //res.redirect('/test');
+    res.end();
+  });
+
+  app.get('/test', function (req, res) {
+   // res.send({message:"welcome"});
+   // res.sendStatus(200);
+    //  res.sendStatus(401);
+    res.attachment('Hello.txt');
+    console.log(res.get('Content-Disposition'));
+  });
+
 //Database connection code
 mongoose.connect(process.env.MONGODBURL,{
      useNewUrlParser: true,
@@ -25,7 +41,7 @@ app.use('/api/v1/user/',user_route)
 app.use('/api/v2/product/',product_route)
 app.use('/api/v3/category/',category_route)
 app.use('/api/v4/cart',cart_route)
-
+app.set('view engine','ejs')
 //listening port
 app.listen(PORT,()=>{
     console.log(`server starting at ${PORT} port... `)
