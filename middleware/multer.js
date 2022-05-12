930cc2a434ba97d25abcd07b088a029c42de9aac
AnithaTatra <102857170+AnithaTatra@users.noreply.github.com>
2022-05-13 01:05:13 +0530

    commited

---
+++ middleware/multer.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+     destination:(req,file,cb)=>{
+         cb(null,'./uploads/')
+     },
+     filename:(req,file,cb)=>{
+         console.log("aaaa...",file.originalname)
+         cb(null,`${file.originalname}`);
+     }
+})
+
+module.exports= 
+{storage:storage};
